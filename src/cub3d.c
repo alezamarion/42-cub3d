541b43d3c1998d72a0b238a197eb1f939ceb562d
alezamarion <alezamarion@icloud.com>
2022-08-15 22:23:06 +0200

    leaks

--- src/cub3d.c
+++ src/cub3d.c
@@ -6,7 +6,7 @@
 /*   By: azamario <azamario@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/11 21:40:09 by azamario          #+#    #+#             */
-/*   Updated: 2022/08/15 21:19:07 by azamario         ###   ########.fr       */
+/*   Updated: 2022/08/15 22:22:26 by azamario         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,19 +20,20 @@ int	main(int argc, char **argv)
 	{
 		game.map = read_map(argv[1]);
 		if (is_valid_map(game.map, argv[1]))
+		{
 			printf("Map is valid\n");
+			free_map(game.map);		
+		}
 		else
+		{
 			printf("Map is not valid\n");
-		exit (0);	
+			free_map(game.map);					
+		}
 	}
 	else
 	{
 		printf("No map specified\n");
 		exit (0);	
 	}
-
-
-
-
-	
+	return (0);
 }
