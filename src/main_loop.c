becdfe1170a968e320973357c83aa5a21dba8ccc
alezamarion <alezamarion@icloud.com>
2022-10-04 22:11:40 -0300

    minimap

--- src/main_loop.c
+++ src/main_loop.c
@@ -6,11 +6,12 @@
 /*   By: azamario <azamario@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/28 17:39:22 by azamario          #+#    #+#             */
-/*   Updated: 2022/10/03 08:22:55 by azamario         ###   ########.fr       */
+/*   Updated: 2022/10/04 22:08:08 by azamario         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/cub3d.h"
+
 
 //Draw the line by DDA algorithm
 void	draw_line(t_game *game, double x1, double y1, double x2, double y2)
@@ -26,7 +27,7 @@ void	draw_line(t_game *game, double x1, double y1, double x2, double y2)
 	deltaY /= step;
 	while (fabs(x2 - x1) > 0.01 || fabs(y2 - y1) > 0.01)
 	{
-		game->img.data[((int)floor(y1) * (game->map_attributes.row * TILE_SIZE) + (int)floor(x1))] = 0xFFFFFF;
+		game->img.data[((int)floor(y1) * game->map_attributes.col * TILE_SIZE + (int)floor(x1))] = 0xFFFFFF;
 		x1 += deltaX;
 		y1 += deltaY;
 	}
@@ -66,7 +67,7 @@ void	draw_rectangle(t_game *game, int x, int y)
 		j = 0;
 		while (j < TILE_SIZE)
 		{
-			game->img.data[(y  + i) * (game->map_attributes.col * TILE_SIZE) + x + j] = 0xD47513;
+			game->img.data[(y + i) * game->map_attributes.col * TILE_SIZE + x + j] = 0x9900FF;
 			j++;
 		}
 		i++;
@@ -84,98 +85,13 @@ void	draw_rectangles(t_game *game)
 		j = 0;
 		while (j < game->map_attributes.col)
 		{
-			if (game->map[i][j] == 1)
+			if (game->map[i][j] == '1')
 				draw_rectangle(game, j, i);
 			j++;
 		}
 		i++;
 	}
 }
-
-
-
-/*
-//Draw the line by DDA algorithm
-void	draw_line(t_game *game, double x1, double y1, double x2, double y2)
-{
-	double	deltaX;
-	double	deltaY;
-	double	step;
-
-	deltaX = x2 - x1;
-	deltaY = y2 - y1;
-	step = (fabs(deltaX) > fabs(deltaY)) ? fabs(deltaX) : fabs(deltaY);
-	deltaX /= step;
-	deltaY /= step;
-	while (fabs(x2 - x1) > 0.01 || fabs(y2 - y1) > 0.01)
-	{
-		game->img.data[TO_COORD(x1, y1)] = 0xb3b3b3;
-		x1 += deltaX;
-		y1 += deltaY;
-	}
-}
-
-void 	draw_lines(t_game *game)
-{
-	int		i;
-	int		j;
-
-	i = 0;
-	while (i < COLS)
-	{
-		draw_line(game, i * TILE_SIZE, 0, i * TILE_SIZE, HEIGHT);
-		i++;
-	}
-	draw_line(game, COLS * TILE_SIZE - 1, 0, COLS * TILE_SIZE - 1, HEIGHT);
-	j = 0;
-	while (j < ROWS)
-	{
-		draw_line(game, 0, j * TILE_SIZE, WIDTH, j * TILE_SIZE);
-		j++;
-	}
-	draw_line(game, 0, ROWS * TILE_SIZE - 1, WIDTH, ROWS * TILE_SIZE - 1);
-}
-
-void	draw_rectangle(t_game *game, int x, int y)
-{
-	int i;
-	int j;
-
-	x *= TILE_SIZE;
-	y *= TILE_SIZE;
-	i = 0;
-	while (i < TILE_SIZE)
-	{
-		j = 0;
-		while (j < TILE_SIZE)
-		{
-			game->img.data[(y  + i) * WIDTH + x + j] = 0x9900FF;
-			j++;
-		}
-		i++;
-	}
-}
-
-void	draw_rectangles(t_game *game)
-{
-	int		i;
-	int		j;
-
-	i = 0;
-	while (i < ROWS)
-	{
-		j = 0;
-		while (j < COLS)
-		{
-			if (game->map[i][j] == 1)
-				draw_rectangle(game, j, i);
-			j++;
-		}
-		i++;
-	}
-}
-*/
-
 
 int		main_loop(t_game *game)
 {
