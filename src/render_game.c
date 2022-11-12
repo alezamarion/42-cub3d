05aeb9d791d3cc0021ac6ffe8d2cd4eb4d1ef104
alezamarion <alezamarion@icloud.com>
2022-11-12 18:44:24 -0300

    draw_line

--- src/render_game.c
+++ src/render_game.c
@@ -6,11 +6,23 @@
 /*   By: azamario <azamario@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/28 17:39:22 by azamario          #+#    #+#             */
-/*   Updated: 2022/10/26 08:42:39 by azamario         ###   ########.fr       */
+/*   Updated: 2022/11/12 18:41:59 by azamario         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/cub3d.h"
+
+void	render_rays(t_game *game)
+{
+	int i;
+
+	i = 0;
+	while (i < NUM_RAYS)
+	{
+		draw_line(game,game->player.posX, game->player.posY, game->rays[i].wall_hit_x, game->rays[i].wall_hit_y);
+	}
+}
+
 
 void	draw_minimap(t_game *game)
 {
@@ -30,6 +42,7 @@ int		render_game(t_game *game)
 	//draw_floor_and_ceiling;
 	draw_minimap(game);
 	draw_player(game);
+	render_rays(game);
 	mlx_put_image_to_window(game->mlx, game->window, game->img.img, 0, 0);
 	return (0);
 }
